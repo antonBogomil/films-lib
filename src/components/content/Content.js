e0c0971c5ad9979d4a1bc9234902a6286687f354
antonBogomil <anton.bogomil777@gmail.com>
2019-03-30 12:27:32 +0200

    need to commit for pull

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import MainPage from '../pages/main';
-import FilmList from '../pages/list/FilmList';
 import './content.scss';
-import NotFound from '../notFound/NotFound';
-import LoginContainer from '../../containers/LoginContainer';
 import Routes from '../router/Routes';
 const Content = ({modalOpen}) => {
 	return (
